be3487ab72595b2a03d242a67a86febc7e42be16
Nillohit-Sharma <88533204+Nillohit-Sharma@users.noreply.github.com>
2022-11-12 11:36:28 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,38 +1,29 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-const path = require('path');
-const cors = require('cors');
-// Cors 
+const express = require('express')
+const app = express()
+const path = require('path')
+const cors = require('cors')
+
+const PORT = process.env.PORT || 3000
+const connectDB = require('./config/db')
+connectDB()
+
 const corsOptions = {
   origin: process.env.ALLOWED_CLIENTS.split(',')
-  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
 }
+app.use(cors(corsOptions));
 
-// Default configuration looks like
-// {
-//     "origin": "*",
-//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     "preflightContinue": false,
-//     "optionsSuccessStatus": 204
-//   }
+app.use(express.json())
+app.use(express.static('public'))
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs')
 
-app.use(cors(corsOptions))
-app.use(express.static('public'));
+app.use('/', require('./routes/home'))
+app.use('/uploaded-files', require('./routes/listing'))
+app.use('/cleanup', require('./routes/cleanup'))
+app.use('/api/files', require('./routes/files'))
+app.use('/files', require('./routes/filePreview'))
+app.use('/files/download', require('./routes/download'))
 
-const connectDB = require('./config/db');
-connectDB();
-
-app.use(express.json());
-
-app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'ejs');
-
-// Routes 
-app.use('/api/files', require('./routes/files'));
-app.use('/files', require('./routes/show'));
-app.use('/files/download', require('./routes/download'));
-
-
-app.listen(PORT, console.log(`Listening on port ${PORT}.`));
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`)
+});
